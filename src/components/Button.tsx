dfd843a3e1bf0d6efd6fbc9201641ba50e5dcc3b
Justin Charles <charlesjustin2124@gmail.com>
2024-11-26 09:57:26 +0530

    Created the Category Section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,51 +1,36 @@
-import { cva, VariantProps } from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-// Define button styles with improved color variants and hover effects
-const buttonStyles = cva(
-  ["transition-colors", "font-medium", "focus:outline-none", "focus:ring"],
-  {
-    variants: {
-      variant: {
-        default: [
-          "bg-blue-400",
-          "text-white",
-          "hover:bg-blue-600",
-          "focus:ring-blue-300",
-        ],
-        ghost: [
-          "bg-transparent",
-          "text-gray-700",
-          "hover:bg-gray-100",
-          "focus:ring-gray-300",
-        ],
-        dark: [
-          "bg-secondary-dark",
-          "hover:bg-secondary-dark-hover",
-          "text-secondary",
-        ],
-      },
-      size: {
-        default: ["rounded", "px-4", "py-2", "text-sm"],
-        large: ["rounded-lg", "px-6", "py-3", "text-base"],
-        icon: [
-          "rounded-full",
-          "w-10",
-          "h-10",
-          "flex",
-          "items-center",
-          "justify-center",
-          "p-2.5",
-        ],
-      },
+export const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-400"],
+      dark: [
+        "bg-secondary-dark",
+        "hover:bg-secondary-dark-hover",
+        "text-secondary",
+      ],
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: [" rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
 
